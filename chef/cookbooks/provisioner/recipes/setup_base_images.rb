c2b3ae23d050e61b5d8b6ed15b7814d64a903472
Victor Lowther <victor_lowther@dell.com>
2011-11-30 14:14:52 -0600

    Use nginx as the provisioner web server.

--- chef/cookbooks/provisioner/recipes/setup_base_images.rb
+++ chef/cookbooks/provisioner/recipes/setup_base_images.rb
@@ -53,22 +53,32 @@ end
 
 include_recipe "bluepill"
 
+package "nginx"
+
 # Set up our the webserver for the provisioner.
 file "/var/log/provisioner-webserver.log" do
   owner "nobody"
   action :create
 end
 
-template "/etc/bluepill/provisioner-webserver.pill" do
+template "/etc/nginx/provisioner.conf" do
+  source "base-nginx.conf.erb"
   variables(:docroot => "/tftpboot",
-            :port =>8091,
-            :appname => "provisioner-webserver",
-            :logfile => "/var/log/provisioner-webserver.log")
-  source "provisioner-webserver.pill.erb"
+            :port => 8091,
+            :logfile => "/var/log/provisioner-webserver.log",
+            :pidfile => "/var/run/provisioner-webserver.pid")
 end
 
 bluepill_service "provisioner-webserver" do
-  action [:load, :enable, :start]
+  variables(:processes => [ {
+                              "daemonize" => false,
+                              "pid_file" => "/var/run/provisioner-webserver.pid",
+                              "start_command" => "nginx -c /etc/nginx/provisioner.conf",
+                              "stderr" => "/var/log/provisioner-webserver.log",
+                              "stdout" => "/var/log/provisioner-webserver.log",
+                              "name" => "provisioner-webserver"
+                            } ] )
+  action [:create, :load]
 end
 
 # Set up the TFTP server as well.
