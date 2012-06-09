3bf6af74453da1aadc77a1a6dc2712b4226a75f7
Victor Lowther <victor.lowther@gmail.com>
2012-06-09 18:20:12 -0500

    Add a helper function to wait for a crowbar state change.
    The main control.sh will not use it, but it will come in handy for hooks.

--- updates/control_lib.sh
+++ updates/control_lib.sh
@@ -9,12 +9,14 @@ parse_node_data() {
         -a crowbar.network.bmc.netmask \
         -a crowbar.network.bmc.address \
         -a crowbar.network.bmc.router \
+        -a state \
         -a crowbar.allocated)
     then
         for s in ${node_data} ; do
             VAL=${s#*=}
             case ${s%%=*} in
                 name) export HOSTNAME=$VAL;;
+                state) export CROWBAR_STATE=$VAL;;
                 crowbar.allocated) export ALLOCATED=$VAL;;
                 crowbar.network.bmc.router) export BMC_ROUTER=$VAL;;
                 crowbar.network.bmc.address) export BMC_ADDRESS=$VAL;;
@@ -24,6 +26,7 @@ parse_node_data() {
         echo "BMC_ROUTER=${BMC_ROUTER}"
         echo "BMC_ADDRESS=${BMC_ADDRESS}"
         echo "BMC_NETMASK=${BMC_NETMASK}"
+        echo "CROWBAR_STATE=${CROWBAR_STATE}"
         echo "HOSTNAME=${HOSTNAME}"
         echo "ALLOCATED=${ALLOCATED}"
     else
@@ -74,18 +77,34 @@ __get_state() {
 post_state() { try_to "$MAXTRIES" 15 __post_state "$@"; }
 get_state() { try_to "$MAXTRIES" 15 __get_state "$@"; }
 
-reboot_system () {
+reboot_system() {
   sync
   sleep 30
   umount -l /updates /install-logs
   reboot
 }
 
-wait_for_allocated () {
+wait_for_allocated() {
     # $1 = hostname
     while [[ $ALLOCATED = false ]]; do
         get_state "$1"
         [[ $ALLOCATED = true ]] && return
+        sleep 15
+    done
+}
+
+wait_for_crowbar_state() {
+    # $1 = hostname
+    # $2 = crowbar state to wait for.  If empty, wait for a state change
+    [[ $2 && $2 = $CROWBAR_STATE ]] && return
+    local current_state=$CROWBAR_STATE
+    while [[ 1 = 1 ]]; do
+        get_state "$1"
+        if [[ $2 ]]; then
+            [[ $2 = $CROWBAR_STATE ]] && return
+        elif [[ $current_state != $CROWBAR_STATE ]]; then
+            return
+        fi
         sleep 15
     done
 }
