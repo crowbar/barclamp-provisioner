77aa0506feffc1d20b4595e6ff15844daf2e7ec7
Bernhard M. Wiedemann <bwiedemann@suse.de>
2012-06-18 18:24:07 +0200

    Write unique MBR disk signatures
    This gives BIOS+Linux+autoyast a hint on which disk is which

--- updates/control.sh
+++ updates/control.sh
@@ -148,6 +148,7 @@ nuke_everything() {
         else
             dd "if=/dev/zero" "of=/dev/$name" "bs=512" "count=$blocks"
         fi
+        echo w | fdisk /dev/$name # write new unique MBR signature
     done < <(tac /proc/partitions)
 }
 
