c1724c698f0f0f044e196aaa7156680f6b1b4555
Christoph Thiel <cthiel@suse.com>
2012-08-25 19:47:07 +0200

    update to control.sh + control_lib.sh from fred branch
    This reverts
    https://github.com/SUSE-Cloud/barclamp-provisioner/commit/042b0f9e8f3f5fb479d0501781a74214a953b651
    for now, which we might need to add back. Need to verify if
    wait_for_crowbar_state could do the job.

--- updates/control.sh
+++ updates/control.sh
@@ -23,20 +23,23 @@ DHCPDIR=/var/lib/dhclient
  DHCPDIR=/var/lib/dhcp
 }
 
-MYINDEX=${MYIP##*.}
-STATE=$(grep -o -E 'crowbar\.state=[^ ]+' /proc/cmdline)
-STATE=${STATE#*=}
-DEBUG=`grep dhcp-client-debug $DHCPDIR/dhclient*.leases | uniq | cut -d" " -f5 | cut -d";" -f1`
-export BMC_ADDRESS=""
-export BMC_NETMASK=""
-export BMC_ROUTER=""
+[[ $DEBUG ]] && set -x
 
-FINAL_STATE_FILE="/tmp/final-state.$$"
+MYINDEX=${MYIP##*.}
+DHCP_STATE=$(grep -o -E 'crowbar\.state=[^ ]+' /proc/cmdline)
+DHCP_STATE=${DHCP_STATE#*=}
+MAXTRIES=5
+BMC_ADDRESS=""
+BMC_NETMASK=""
+BMC_ROUTER=""
+ALLOCATED=false
+export DHCP_STATE MYINDEX BMC_ADDRESS BMC_NETMASK BMC_ROUTER ADMIN_IP
+export ALLOCATED HOSTNAME CROWBAR_KEY CROWBAR_STATE
 
 # Make sure date is up-to-date
 if [ ! -f /etc/SuSE-release ]
 then
-  while ! /usr/sbin/ntpdate $ADMIN_IP
+  until /usr/sbin/ntpdate $ADMIN_IP || [[ $DHCP_STATE = 'debug' ]]
   do
     echo "Waiting for NTP server"
     sleep 1
@@ -56,16 +59,12 @@ fi
 killall dhclient
 killall dhclient3
 
-
 if [ ! -f /etc/SuSE-release ]
 then
   # HACK fix for chef-client
-  if [ -e /root/rest-client*gem ]
-  then
-    pushd /root
-    gem install --local rest-client
-    popd
-  fi
+  cd /root
+  gem install --local rest-client
+  cd -
 
   # Other gem dependency installs.
   cat > /etc/gemrc <<EOF
@@ -73,7 +72,6 @@ then
 - http://$ADMIN_IP:8091/gemsite/
 gem: --no-ri --no-rdoc --bindir /usr/local/bin
 EOF
-  gem install rest-client
   gem install xml-simple
   gem install libxml-ruby
   gem install wsman
@@ -94,45 +92,7 @@ curl -L -o /etc/chef/validation.pem \
     --connect-timeout 60 -s \
     "http://$ADMIN_IP:8091/validation.pem"
 
-parse_node_data() {
-  for s in $(/tmp/parse_node_data -a name -a crowbar.network.bmc.netmask -a crowbar.network.bmc.address -a crowbar.network.bmc.router -a crowbar.allocated $1) ; do
-    VAL=${s#*=}
-    case ${s%%=*} in
-      name) export HOSTNAME=$VAL;;
-      crowbar.allocated) export NODE_STATE=$VAL;;
-      crowbar.network.bmc.router) export BMC_ROUTER=$VAL;;
-      crowbar.network.bmc.address) export BMC_ADDRESS=$VAL;;
-      crowbar.network.bmc.netmask) export BMC_NETMASK=$VAL;;
-    esac
-  done
-
-  echo BMC_ROUTER=${BMC_ROUTER}
-  echo BMC_ADDRESS=${BMC_ADDRESS}
-  echo BMC_NETMASK=${BMC_NETMASK}
-  echo HOSTNAME=${HOSTNAME}
-  echo NODE_STATE=${NODE_STATE}
-}
-
-
-post_state() {
-  local curlargs=(-o "/tmp/node_data.$$" --connect-timeout 60 -s \
-      -L -X POST --data-binary "{ \"name\": \"$1\", \"state\": \"$2\" }" \
-      -H "Accept: application/json" -H "Content-Type: application/json")
-  [[ $CROWBAR_KEY ]] && curlargs+=(-u "$CROWBAR_KEY" --digest --anyauth)
-  curl "${curlargs[@]}" "http://$ADMIN_IP:3000/crowbar/crowbar/1.0/transition/default"
-  parse_node_data /tmp/node_data.$$
-  rm /tmp/node_data.$$
-  echo "$2" > $FINAL_STATE_FILE
-}
-
-get_state() {
-    local curlargs=(-o "/tmp/node_data.$$" --connect-timeout 60 -s \
-      -L -H "Accept: application/json" -H "Content-Type: application/json")
-  [[ $CROWBAR_KEY ]] && curlargs+=(-u "$CROWBAR_KEY" --digest)
-  curl "${curlargs[@]}" "http://$ADMIN_IP:3000/crowbar/machines/1.0/show?name=$HOSTNAME"
-  parse_node_data /tmp/node_data.$$
-  rm /tmp/node_data.$$
-}
+. "/updates/control_lib.sh"
 
 nuke_everything() {
     # Make sure that the kernel knows about all the partitions
@@ -153,85 +113,89 @@ nuke_everything() {
         fi
         echo w | fdisk /dev/$name # write new unique MBR signature
     done < <(tac /proc/partitions)
+
+    ## for good measure, nuke partition tables on disks (nothing should remain bootable)
+    # for i in `ls /dev/sd?`; do  parted -m -s  $i mklabel bsd ; sleep 1 ; done
 }
 
-run_chef () {
-  chef-client -S http://$ADMIN_IP:4000/ -N $1
-}
-
-case $STATE in
-    discovery)  
-        echo "Discovering with: $HOSTNAME_MAC"
-        post_state $HOSTNAME_MAC discovering
-        run_chef $HOSTNAME_MAC
-        post_state $HOSTNAME_MAC discovered
-
-        while [ "$NODE_STATE" != "true" ] ; do
-          sleep 15
-          get_state
-        done
-
-        echo "Hardware installing with: $HOSTNAME"
-        rm -f /etc/chef/client.pem
-        post_state $HOSTNAME hardware-installing
-        nuke_everything
-        run_chef $HOSTNAME
-        if [ -a /var/log/chef/hw-problem.log ]; then
-          post_state $HOSTNAME problem
-        else          
-          post_state $HOSTNAME hardware-installed
-        fi
-        nuke_everything;;
-    hwinstall)  
-        while [ "$NODE_STATE" != "true" ] ; do
-            sleep 15
-            get_state
-        done
-
-        post_state $HOSTNAME hardware-installing
-        nuke_everything
-        echo "Hardware installing with: $HOSTNAME"
-        run_chef $HOSTNAME
-        if [ -a /var/log/chef/hw-problem.log ]; then
-            post_state $HOSTNAME problem
-        else          
-            post_state $HOSTNAME hardware-installed
-        fi
-        nuke_everything;;
-    update)  
-        post_state $HOSTNAME hardware-updating
-        run_chef $HOSTNAME
-        if [ -a /var/log/chef/hw-problem.log ]; then
-            post_state $HOSTNAME problem
-        else          
-            post_state $HOSTNAME hardware-updated
-        fi;;
-esac 2>&1 | tee -a /install-logs/$HOSTNAME-update.log
-if [[ $DEBUG != 1 && $DEBUG != true ]]; then
-    sync
-    sleep 30
-    case "$(cat $FINAL_STATE_FILE 2>/dev/null)" in
-	hardware-installed|hardware-updated)
-            # If we've transitioned to one of these states, there needs to be a
-            # link for pxe boot for this MAC address.  Without it, we'll just
-            # reboot into discovery again and get "stuck".  This can happen if
-            # the admin node is very slow updating pxe config.  So just in case
-            # we'll poll here for up to five minutes before giving up and just
-            # rebooting
-            let pc=0
-            pxe_link="01-$(echo $MAC | tr '[:upper:]:' '[:lower:]-')"
-            pxe_link="http://$ADMIN_IP:8091/discovery/pxelinux.cfg/$pxe_link"
-            while ! curl -s --head $pxe_link | head -n 1 | grep -q 'HTTP.*200' ; do
-                echo "$pxe_link not found, waiting..." | tee -a /install-logs/$HOSTNAME-update.log
-                sleep 10
-                let pc=pc+1
-                [ $pc -gt 30 ] && {
-                    echo "$pxe_link still not found, giving up" | tee -a /install-logs/$HOSTNAME-update.log
-                    break
-                }
-            done
-        ;;
+# If there are pre/post transition hooks for this state (per system or not),
+# handle them.
+run_hooks() {
+    # $1 = hostname
+    # $2 = state
+    # $3 = pre or post
+    local hookdirs=() hookdir='' hook=''
+    # We only handle pre and post hooks.  Anything else is a bug in
+    # control.sh that we should debug.
+    case $3 in
+        pre) hookdirs=("/updates/$1/$2-$3" "/updates/$2-$3");;
+        post) hookdirs=("/updates/$2-$3" "/updates/$1/$2-$3");;
+        *) post_state "$1" debug; reboot_system;;
     esac
-    umount /updates /install-logs
-    reboot
-fi
+    for hookdir in "${hookdirs[@]}"; do
+        [[ -d $hookdir ]] || continue
+        for hook in "$hookdir/"*.hook; do
+            [[ -x $hook ]] || continue
+            # If a hook fails, then Something Weird happened, and it
+            # needs to be debugged.
+            export HOOKSTATE="$2-$3" HOOKNAME="${hook##*/}"
+            if "$hook"; then
+                unset HOOKSTATE HOOKNAME
+                get_state "$1"
+                continue
+            else
+                post_state "$1" debug
+                reboot_system
+            fi
+        done
+    done
+}
+
+walk_node_through () {
+    # $1 = hostname for chef-client run
+    # $@ = states to walk through
+    local name="$1" f='' state=''
+    shift
+    while (( $# > 1)); do
+        state="$1"
+        post_state "$name" "$1"
+        run_hooks "$HOSTNAME" "$1" pre
+        chef-client -S http://$ADMIN_IP:4000/ -N "$name"
+        run_hooks "$HOSTNAME" "$1" post
+        shift
+    done
+    state="$1"
+    run_hooks "$HOSTNAME" "$1" pre
+    report_state "$name" "$1"
+    run_hooks "$HOSTNAME" "$1" post
+}
+
+# If there is a custom control.sh for this system, source it.
+[[ -x /updates/$HOSTNAME/control.sh ]] && \
+    . "/updates/$HOSTNAME/control.sh"
+
+discover() {
+    echo "Discovering with: $HOSTNAME_MAC"
+    walk_node_through $HOSTNAME_MAC discovering discovered
+}
+
+hardware_install () {
+    wait_for_allocated "$HOSTNAME"
+    echo "Hardware installing with: $HOSTNAME"
+    rm -f /etc/chef/client.pem
+    nuke_everything
+    walk_node_through $HOSTNAME hardware-installing hardware-installed
+    nuke_everything
+}
+
+hwupdate () {
+    walk_node_through $HOSTNAME hardware-updating hardware-updated
+}
+
+case $DHCP_STATE in
+    discovery) discover && hardware_install;;
+    hwinstall) hardware_install;;
+    update) hwupdate;;
+esac 2>&1 | tee -a /install-logs/$HOSTNAME-update.log
+[[ $DHCP_STATE = 'debug' ]] && exit
+reboot_system
