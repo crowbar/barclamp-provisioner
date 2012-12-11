fe218fff6d99ea92bf1d9da6e2825520c0a4179f
Victor Lowther <victor.lowther@gmail.com>
2012-12-11 16:27:22 -0600

    Pretty up bootdisk finding, and put the code in a ruby_block to make it run last.

--- chef/cookbooks/provisioner/recipes/bootdisk.rb
+++ chef/cookbooks/provisioner/recipes/bootdisk.rb
@@ -1,39 +1,35 @@
 #!/usr/bin/env ruby
 
-basedir="/dev/disk/by-path"
-dev=nil
 node[:crowbar_wall] ||= Mash.new
 
 # Find the first thing that looks like a hard drive based on
 # PCI bus enumeration, and use it as the target disk.
 # Unless some other barclamp has set it, that is.
-return if node[:crowbar_wall][:boot_device]
-::Dir.entries(basedir).sort.each do |path|
-  # Not a symlink?  Not interested.
-  next unless File.symlink?("#{basedir}/#{path}")
-  # Symlink does not point at a disk?  Also not interested.
-  dev = File.readlink("#{basedir}/#{path}").split('/')[-1]
-  break if dev =~ /^[hsv]d[a-z]+$/
-  dev = nil
-end
-
-raise "Cannot find a hard disk!" unless dev
-# Turn the found device into its corresponding /dev/disk/by-id link.
-# This name shoule be more stable than the /dev/disk/by-path one.
-basedir="/dev/disk/by-id"
-bootdisk=::Dir.entries(basedir).select do |m|
-  f="#{basedir}/#{m}"
-  File.symlink?(f) && (File.readlink(f).split('/')[-1] == dev)
-end.sort do |a,b|
-  case
-  when a == b then 0
-  when a =~ /^scsi-/ then -1
-  when b =~ /^scsi-/ then 1
-  when a =~ /^ata-/ then -1
-  when b =~ /^ata-/ then 1
-  else a <=> b
+ruby_block "Find the fallback boot device" do
+  block do
+    basedir="/dev/disk/by-path"
+    dev=nil
+    ::Dir.entries(basedir).sort.each do |path|
+      # Not a symlink?  Not interested.
+      next unless File.symlink?("#{basedir}/#{path}")
+      # Symlink does not point at a disk?  Also not interested.
+      dev = File.readlink("#{basedir}/#{path}").split('/')[-1]
+      break if dev =~ /^[hsv]d[a-z]+$/
+      dev = nil
+    end
+    raise "Cannot find a hard disk!" unless dev
+    # Turn the found device into its corresponding /dev/disk/by-id link.
+    # This name shoule be more stable than the /dev/disk/by-path one.
+    basedir="/dev/disk/by-id"
+    bootdisks=::Dir.entries(basedir).select do |m|
+      f="#{basedir}/#{m}"
+      File.symlink?(f) && (File.readlink(f).split('/')[-1] == dev)
+    end
+    bootdisk = bootdisks.find{|b|b =~ /^scsi-/} ||
+      bootdisks.find{|b|b =~ /^ata-/} ||
+      bootdisks.first
+    node[:crowbar_wall][:boot_device] = "disk/by-id/#{bootdisk}"
+    node.save
   end
-end.first
-node[:crowbar_wall][:boot_device] = "disk/by-id/#{bootdisk}"
-
-node.save
+  not_if do node[:crowbar_wall][:boot_device] end
+end
