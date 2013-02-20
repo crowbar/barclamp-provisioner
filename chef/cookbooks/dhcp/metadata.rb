42f82ffc2f5ebead9eac8ed4eeae773c3507054e
Victor Lowther <victor.lowther@gmail.com>
2013-02-20 11:53:55 -0600

    Add metadata.rb for the DHCP cookbook

---
+++ chef/cookbooks/dhcp/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Dell Crowbar Team"
+maintainer_email "crowbar@dell.com"
+license          "Apache 2.0"
+description      "Maintains DHCP server for Crowbar"
+long_description "Maintains DHCP server for Crowbar"
+version          "1.0.0"
+
