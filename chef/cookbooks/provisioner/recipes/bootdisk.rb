4db3513f0c0697226503754e25651fc9d0d79172
Vincent Untz <vuntz@suse.com>
2013-06-05 00:01:42 +0200

    bootdisk: Save boot device by path, instead of by device if no id
    The path is still more stable than the device name.

--- chef/cookbooks/provisioner/recipes/bootdisk.rb
+++ chef/cookbooks/provisioner/recipes/bootdisk.rb
@@ -9,16 +9,19 @@ ruby_block "Find the fallback boot device" do
   block do
     basedir="/dev/disk/by-path"
     dev=nil
+    disk_by_path = nil
     ::Dir.entries(basedir).sort.each do |path|
       # Not a symlink?  Not interested.
       next unless File.symlink?("#{basedir}/#{path}")
       # Symlink does not point at a disk?  Also not interested.
       dev = File.readlink("#{basedir}/#{path}").split('/')[-1]
+      disk_by_path = "disk/by-path/#{path}"
       break if dev =~ /^[hsv]d[a-z]+$/
       dev = nil
+      disk_by_path = nil
     end
     raise "Cannot find a hard disk!" unless dev
-    node[:crowbar_wall][:boot_device] = dev
+    node[:crowbar_wall][:boot_device] = disk_by_path
     # Turn the found device into its corresponding /dev/disk/by-id link.
     # This name should be more stable than the /dev/disk/by-path one.
     basedir="/dev/disk/by-id"
