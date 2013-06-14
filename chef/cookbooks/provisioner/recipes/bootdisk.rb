9ad1231b68b5f1ba96644426c08b0e99c8991afe
Victor Lowther <victor.lowther@gmail.com>
2013-06-14 10:05:11 -0500

    Fix up the bootdev recipe to rely exclusively on sysfs to find our boot device.
    This code goes to great lengths to make sure that things do not get
    confused by SCSI WWN identifiers.
    
    Also include code to make GRUB also try to install our root fs device.
    This code may be nonfunctional due to glitches in the debian installer code.

--- chef/cookbooks/provisioner/recipes/bootdisk.rb
+++ chef/cookbooks/provisioner/recipes/bootdisk.rb
@@ -2,44 +2,71 @@
 
 node[:crowbar_wall] ||= Mash.new
 
-# Find the first thing that looks like a hard drive based on
-# PCI bus enumeration, and use it as the target disk.
-# Unless some other barclamp has set it, that is.
 ruby_block "Find the fallback boot device" do
   block do
-    basedir="/dev/disk/by-path"
-    dev=nil
-    disk_by_path = nil
-    ::Dir.entries(basedir).sort.each do |path|
-      # Not a symlink?  Not interested.
-      next unless File.symlink?("#{basedir}/#{path}")
-      # Symlink does not point at a disk?  Also not interested.
-      dev = File.readlink("#{basedir}/#{path}").split('/')[-1]
-      disk_by_path = "disk/by-path/#{path}"
-      break if dev =~ /^[hsv]d[a-z]+$/
-      dev = nil
-      disk_by_path = nil
+    # Parse what sysfs knows about all our block devices
+    # in order to find our boot device candidate.
+    # We do it this way instead of using /dev/disk/by-path
+    # because udev can identify devices by SCSI WWN intead of
+    # host:bus:target:lun identification.
+    # This will break for devices that are not addressed through
+    # the SCSI midlayer.
+    Bootdev = Struct.new(:device,:pci_address,:scsi_id, :path)
+    candidates = Array.new
+    ::Dir.entries("/sys/block").each do |dev|
+      # Pre-filter out uninteresting devices.
+      next unless dev =~ /^[hsv]d[a-z]+$/
+      name = File.join("/sys/block",dev)
+      next unless File.symlink?(name)
+      link = File.readlink(name)
+      path = File.expand_path(link,"/sys/block")
+      # Ignore USB devices
+      next if path =~ /\/usb[^\/]*\//
+      # Ignore any non-removable USB devices
+      next if (node[:block_device][dev][:removable] rescue "not here") != "0"
+      pci_address = Array.new
+      scsi_id = Array.new
+      # For now, we are looking for devices hosting SCSI devices hanging off
+      # devices attached to a PCI bus.  This means that iSCSI targets and
+      # things that do not communicate through the SCSI midlayer will not
+      # work, but those are the breaks for now.
+      # A better algorithm is welcome.
+      path.split('/').each do |p|
+        case
+        when /(([0-9a-f]+:){2}[0-9a-f]+\.[0-9a-f]+)/ =~ p
+          # We need to handle it this way to account for multiple layers
+          # of PCI busses.  It would not do to pick the wrong device due
+          # to not mapping out the PCI bus topology properly.
+          pci_address << $1.split(/[:\.]/).map{|i|i.hex}
+        when /(([0-9a-f]+:){3}[0-9a-f]+)/ =~ p
+          # It is not likely that we will have more than one SCSI quad
+          # in a sysfs path, but it should be harmless if we do.
+          scsi_id << $1.split(":").map{|i|i.hex}
+        end
+      end
+      next if pci_address.empty? || scsi_id.empty?
+      Chef::Log.info("Will consider #{path} as the boot device.")
+      candidates << Bootdev.new(dev,pci_address,scsi_id,path)
     end
-    raise "Cannot find a hard disk!" unless dev
-    node[:crowbar_wall][:boot_device] = disk_by_path
-    # Turn the found device into its corresponding /dev/disk/by-id link.
-    # This name shoule be more stable than the /dev/disk/by-path one.
-    
-    basedir="/dev/disk/by-id"
-    if File.exists? basedir
-      bootdisks=::Dir.entries(basedir).select do |m|
-        f="#{basedir}/#{m}"
-        File.symlink?(f) && (File.readlink(f).split('/')[-1] == dev)
-      end
-      unless bootdisks.empty?
-        bootdisk = bootdisks.find{|b|b =~ /^scsi-/} ||
-          bootdisks.find{|b|b =~ /^ata-/} ||
-          bootdisks.first
-        node[:crowbar_wall][:boot_device] = "disk/by-id/#{bootdisk}"
-      end
+    # Find the first thing that looks like a hard drive based on
+    # PCI bus enumeration and SCSI quad, and use it as the target disk.
+    # Unless some other barclamp has set it, that is.
+    candidate = candidates.sort do |a,b|
+      res = a.pci_address <=> b.pci_address
+      res = a.scsi_id <=> b.scsi_id if res == 0
+      res = a.device <=> b.device if res == 0
+      res
+    end.first
+    dev = "sda"
+    if !candidate.nil?
+      Chef::Log.info("Picked #{candidate.path} as the boot device.")
+      dev = candidate.device
+    else
+      Chef::Log.info("Could not pick a best candidate, defaulting to /dev/sda")
     end
     disk = BarclampLibrary::Barclamp::Inventory::Disk.new(node,dev)
-    disk.claim("Boot")
+    raise "Could not claim a boot device!" unless disk.claim("Boot")
+    node[:crowbar_wall][:boot_device] = disk.unique_device
     node.save
   end
   not_if do node[:crowbar_wall][:boot_device] end
