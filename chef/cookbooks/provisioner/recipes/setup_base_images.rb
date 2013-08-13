6003fff307ee1abd61fa486b09e17b80a5a772ec
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-08-13 12:08:39 +0400

    create pxecfg_dir directory

--- chef/cookbooks/provisioner/recipes/setup_base_images.rb
+++ chef/cookbooks/provisioner/recipes/setup_base_images.rb
@@ -85,6 +85,11 @@ end
 append_line = append_line.split.join(' ')
 node[:provisioner][:sledgehammer_append_line] = append_line
 
+directory pxecfg_dir do
+  recursive true
+  action :create
+end
+
 template "#{pxecfg_dir}/default" do
   mode 0644
   owner "root"
